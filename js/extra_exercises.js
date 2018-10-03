57d8e5279a2446ec7513341dc3aed01c7880e155
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-10-03 06:44:58 -0500

    Extra functions work

---
+++ js/extra_exercises.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// var inputString = prompt("Please Insert a string here");
+//
+// do {
+//     var inputString = prompt("Please Insert a string here");
+//
+// } while (inputString === "" || isNaN(inputString) === true);
+//
+//
+// alert("This is the string you inputted" + " " + inputString);
+// alert("The lenght of your string was" + " " + inputString.length);
+//
+// var noun = prompt("Please enter a noun");
+// var verb = prompt("Please enter a verb");
+// var adjective = prompt("Please enter a adjective");
+// var adverb = prompt("Please enter a adverb");
+// alert("The" + " " + noun + " " + verb + " " + adjective + " " + adverb);
+
+// var firstNumber = prompt("What is the first number?");
+// var secondNumber = prompt("What is the second number?");
+// var sumNumber = parseFloat(firstNumber) + parseFloat(secondNumber);
+// var subNumber = firstNumber - secondNumber;
+// var dividNumber = firstNumber / secondNumber;
+// var multiNumber = firstNumber * secondNumber;
+//Redo this One
+//
+// alert(firstNumber + " + " + secondNumber + " = " + sumNumber);
+// alert(firstNumber + " - " + secondNumber + " = " + subNumber);
+// alert(firstNumber + " / " + secondNumber + " = " + dividNumber);
+// alert(firstNumber + " * " + secondNumber + " = " + multiNumber);
+
+// var roomLength = prompt("What is the length of the room in feet?");
+// var roomWidth = prompt("What is the width of the room in feet?");
+// var roomAreaImperial = (roomLength * roomWidth);
+// const notPi = 0.09290304
+// var roomAreaMetric = (roomAreaImperial) * notPi;
+// // var roomAreaMetricSquared = roomAreaMetric / roomAreaMetric;
+// alert("You entered dimensions of " + roomLength + " by " + roomWidth);
+// alert("The area of the room is " + (roomAreaImperial) + " in Imperial");
+// alert("The area of the room is " + (roomAreaMetric) + " in Metric");
+
+// var peopleAtParty = prompt("How many people are at the party?");
+// var howManyPizzaAtParty = prompt("How many pizzas do you have for this here party?");
+// alert("You have " + parseFloat(peopleAtParty) + " people at the party and " + parseFloat(howManyPizzaAtParty) + " Pizzas" );
+// const piecesPerPizza = 8;
+// var piecesAtParty = (piecesPerPizza * howManyPizzaAtParty);
+// var piecesPerPerson =  piecesAtParty / parseFloat(peopleAtParty);
+// alert("You have " + parseInt(piecesPerPerson) + " slices per person at the party");
+// var totalPiecesOfPizza = parseInt(piecesPerPerson) * peopleAtParty;
+// var leftoverPizza = (piecesAtParty - totalPiecesOfPizza);
+// alert("You have " + leftoverPizza + " leftover pieces.");
+
+
+
+
+
