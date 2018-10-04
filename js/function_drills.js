a430947879ab42a1f16a81fd1de4310a0847ba6c
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-10-04 09:20:50 -0500

    Function Excersises

--- js/function_drills.js
+++ js/function_drills.js
@@ -83,6 +83,141 @@ function reverse(input) {
 
 function isNumeric(input) {
     var output = false;
-    output = input === 
+    output = isNaN(input) === false;
     return output;
 }
+
+//Could redo with taking out whitespace
+function count(input) {
+    var output = 'string';
+    output = input.length;
+    return output;
+}
+
+//could redo, make any input into a number
+function add(a,b) {
+    var output = 0;
+    output = a + b;
+    return output;
+}
+
+function subtract(a,b) {
+    var output = 0;
+    output = a - b;
+    return output;
+}
+
+function multiply(a,b) {
+    var output = 0;
+    output = a * b;
+    return output;
+}
+
+function divide(numerator, denominator) {
+    var output = 0;
+    output = numerator / denominator;
+    return output;
+}
+
+function remainder(number, divisor) {
+    var output = 0;
+    output = number % divisor;
+    return output;
+}
+
+function square(a) {
+    var output = 0;
+    output = a * a;
+    return output;
+}
+
+function sumOfSquares(a,b) {
+    var output = 0;
+    output = add(square(a),square(b));
+    return output;
+}
+
+function doMath(operator,a,b) {
+    var output = 0;
+    output = operator(a,b);
+    return output;
+}
+
+function isOdd(number) {
+    var output = true;
+    output = (number % 2 === 1);
+    return output;
+}
+
+function isEven(number) {
+    var output = true;
+    output = (number % 2 === 0);
+    return output;
+}
+
+function identity(input) {
+    var output = 0;
+    output = input;
+    return output;
+}
+
+function isFive(input) {
+    var output = true;
+    output = input === 5;
+    return output;
+}
+
+function addFive(input) {
+    var output = 0;
+    output = (input + 5);
+    return output;
+}
+
+function isMultipleOfFive(input) {
+    var output = 0;
+    output = input % 5 === 0;
+    return output;
+}
+
+function isThree(input) {
+    var output = true;
+    output = input === 3;
+    return output;
+}
+
+function isMultipleOThree(input) {
+    var output = true;
+    output = input % 3 === 0;
+    return output;
+}
+
+function isMultipleOThreeAndFive(input) {
+    var output = true;
+    output = input % 3 === 0 || input % 5 === 0;
+    return output;
+}
+
+function isTrue(input) {
+    var output = true;
+    output = input === true;
+    return output;
+}
+
+function isFalse(input) {
+    var output = true;
+    output = input === false;
+    return output;
+}
+
+function isTruthy(input) {
+    var output = 0;
+    output = input == ;
+    return output;
+}
+
+function isFalsy(input) {
+    var output = true;
+    output = input == true;
+    return output;
+}
+
