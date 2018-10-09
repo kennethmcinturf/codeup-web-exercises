02aed979ba560cd4e4b8accf67de1cd903e386cf
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-10-09 15:58:55 -0500

    Need to finish bonuses

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,10 +1,23 @@
-// var numberToSkip = prompt("what number do you want to skip");
+// var correct = false;
 //
-// for(var n = 1; n < 50; n = n + 2){
-//    if (n  === parseFloat(numberToSkip)){
-//        console.log("Yikes, skip this number");
-//        continue;
-//        }
+// do {
+//     var skipNum = parseFloat(prompt("Enter an odd number between 1 - 50: "));
+//     if (!isNaN(skipNum) && skipNum % 2 !==0) {
+//         correct = true;
+//         break;
+//     }
+// } while(!correct);
 //
-//     console.log("Here is an odd number " + n);
+// console.log("Your number is: " + skipNum);
+// for(i = 1; i <= 50; i+=1) {
+//     if(i % 2 !== 0) {
+//         if(i === skipNum) {
+//             console.log("Yikes, we will be skipping: " + skipNum);
+//             continue;
+//         }
+//         console.log("Here is an odd number " + i);
+//     }
 // }
+
+
+
--- js/loop_bonus.js
+++ js/loop_bonus.js
@@ -10,10 +10,58 @@ function toggle(input) {
     console.log(stringSplit.join(''))
 }
 
+//trying flipping the string
+
 function numberEqualLength(input) {
     var lengthOfString = input.length;
+    var stringSplit = input.split('');
     for (var i = 0; i < input.length; i++)
-        if (i === parseFloat(input)){
+        if (stringSplit[i] === parseInt(stringSplit[i])){
+            var isItSame = parseFloat(stringSplit[i]) === parseInt(lengthOfString);
+        }
+        console.log(isItSame);
+}
+
+//redo your own way
+
+// function getVowels(input) {
+//     var m = input.match(/[aeiou]/gi);
+//     return m === null ? 0 : m.length;
+// }
+
+function getVowels(input) {
+    var vowels = ['a','e','i','o','u'];
+    var stringSplit = input.split('');
+    var result = 0;
+    for (var n = 0; n < input.length; n++){
+        if (stringSplit[n] === vowels){
+            result.push(stringSplit[n]);
+        }
+    }
+    console.log()
+}
+
+function count9(input) {
+    var stringSplit = input.split('');
+    var result = 0;
+    for (var i = 0; i < input.length; i++){
+        if (parseInt(stringSplit[i]) == 9){
+            result.push(stringSplit[i]);
+        }
+    }
+    console.log(result)
+}
+
+function integersOneThroughHundred() {
+    for (var i = 1; i < 101; i++){
+        if (i = 1){
+            console.log(i);
+        }else{
 
         }
+    }
 }
+
+
+
+
