725d1af566fe22e13581c46e8f8e0cd7c0903dfa
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-10-10 08:47:58 -0500

    Completed Excersises(pre-class)

--- js/iterating.js
+++ js/iterating.js
@@ -6,29 +6,39 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+    var names = ['Stephan','Benny','Pebo','David'];
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+    console.log(names.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+    // console.log(names[0]);
+    // console.log(names[1]);
+    // console.log(names[2]);
+    // console.log(names[3]);
 
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
+    // for (var i = 0; i < 4; i++){
+    //    console.log(names[i]);
+    // }
 
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+    // names.forEach(function (name) {
+    //     console.log(name);
+    // });
 
     /**
      * TODO:
@@ -43,5 +53,19 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+    function first(input) {
+        return input[0];
+    }
+    console.log(first(names));
+
+    function second(input) {
+        return input[1];
+    }
+    console.log(second(names));
+
+    function last(input) {
+        return input[input.length - 1];
+    }
+    console.log(last(names));
 
 })();
