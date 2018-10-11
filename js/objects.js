f0890be72ded3ebcfcc8d012890a36bfaed7494b
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-10-11 16:57:29 -0500

    Need to work on Object Bonus

--- js/objects.js
+++ js/objects.js
@@ -11,6 +11,12 @@
      *  > console.log(person.firstName) // "Rick"
      *  > console.log(person.lastName) // "Sanchez"
      */
+    var person = {};
+    person.firstName = "Kenneth";
+    person.lastName = "McInturf";
+
+    console.log(person.firstName);
+    console.log(person.lastName);
 
     /**
      * TODO:
@@ -21,6 +27,11 @@
      * Example
      * > console.log(person.sayHello()) // "Hello from Rick Sanchez!"
      */
+    person.sayHello = function () {
+        console.log("Hello from " + person.firstName + " " + person.lastName);
+    };
+
+    console.log(person.sayHello());
 
     /** TODO:
      * HEB has an offer for the shoppers that buy products amounting to
@@ -36,11 +47,26 @@
      * and console.log the relevant messages for each person
      */
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+    var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+    ];
+
+    function hebDiscount(shopper) {
+        shopper.forEach(function (shoppers) {
+            if (shoppers.amount > 200){
+                console.log(shoppers.name + " original price was $" + shoppers.amount);
+                console.log(shoppers.name + " price after discount was $" + parseFloat(shoppers.amount * .88).toFixed(2));
+                console.log("Their discount was $" + parseFloat(shoppers.amount * .12).toFixed(2));
+            }else {
+                console.log(shoppers.name + " original price was $" + parseFloat(shoppers.amount).toFixed(2));
+                console.log(shoppers.name + "Did not receive a discount");
+            }
+        })
+    }
+
+    console.log(hebDiscount(shoppers));
 
     /** TODO:
      * Create an array of objects that represent books and store it in a
@@ -54,7 +80,33 @@
      * > console.log(books[0].author.firstName) // "Douglas"
      * > console.log(books[0].author.lastName) // "Adams"
      */
-
+    var books = [
+        {title : "Bible",
+        author:{
+            firstName: "Jesus",
+            lastName: "Christ"
+        }},
+        {title : "Lord of the Rings",
+            author:{
+                firstName: "JRR",
+                lastName: "Rowlings"
+        }},
+        {title : "Harry Potter",
+            author:{
+                firstName: "JK",
+                lastName: "Tolkien"
+        }},
+        {title : "Twilight",
+            author:{
+                firstName: "Stephanie",
+                lastName: "Meyer"
+        }},
+        {title : "Davinci Code",
+            author:{
+                firstName: "Dan",
+                lastName: "Brown"
+        }}
+    ];
     /**
      * TODO:
      * Loop through the books array and output the following information about
@@ -79,6 +131,16 @@
      *      ---
      *      ...
      */
+    // function bookInfo(input) {
+    //     books.forEach(function (element, index) {
+    //         console.log("Book #" + (index + 1) + "\n");
+    //         console.log("Title: " + element.title + "\n");
+    //         console.log("Author: " + element.author.firstName + " " + element.author.lastName + "\n");
+    //         console.log("--");
+    //     })
+    // }
+    //
+    // console.log(bookInfo(books));
 
     /**
      * Bonus:
@@ -91,4 +153,122 @@
      *   `showBookInfo` function.
      */
 
+    function createBook(title,author) {
+        return{
+            title: title,
+            author:{
+                firstName : author.split(' ')[0],
+                lastName : author.split(' ')[1]
+            }
+        }
+    }
+
+    console.log(createBook("Bible", 'Jesus Christo'));
+    // function createBook(title,authorFirstName,authorLastName) {
+    //     var books = {};
+    //     books.title = title;
+    //     books.author.firstName = authorFirstName;
+    //     books.author.lastName = authorLastName;
+    // }
+    //
+    // var books = [];
+
+
+
 })();
+
+/*
+ * Title: JS Objects Practice
+ * Author: Ryan Orsinger
+ * Topics: JS Objects, arrays, writing methods, iteration
+ */
+
+
+// Exercise 1. Write a function called makeCar that takes in two strings, make and model, and returns an object with those properties
+// Example: makeCar("VW", "beetle") returns the object {"make": "VW", "model", "beetle"}
+
+function makeCar(make,model) {
+    var car = {};
+    car.make = make;
+    car.model = model;
+    return car;
+}
+
+// Exercise 2. Use the following data to create an object named salesReport.
+//     The object should have the following properties:
+//       title, date, office, and employees.
+//       the employees property should hold an array of objects.
+//       each employee should be represented by an object with the following properties:
+//         employee-number, first-name, last-name, sales-units
+// 
+// Monthly Sales Report
+// Date: 03-17-2015
+// Office: Codeup
+// ===================================================
+// Employee Number, First Name, Last Name, Sales Units
+// ***************************************************
+// 1, Jane, Janeway, 3
+// 3, Tricia, Triciason, 5
+// 4, Jeannette, Jeanson, 4
+// 5, Charles Emmerson III, Winchester, 2
+// 6, Chet, Chedderson, 8
+// 7, Chaiam, Chaiamson, 12
+// 8, Dale, Dalesinger, 1
+// 9, Zig, Ziglar, 50
+// 10, Henry, Kissinger, 1
+// 11, Arthur Herbert, Fonzarelli, 23
+// 12, Betty, Boop, 67
+
+var salesReport = {
+    "Employees": [""]
+};
+
+
+// Exercise 3. On the salesReport object you made, add the following methods.
+// getEmployeeCount() should return the total number of employees
+// getTotalNumberOfSales() should return the total number of units sold
+// getAverageSalesPerEmployee() should return the average units sold per employee
+
+
+// Exercise 4. Go to https://gist.githubusercontent.com/ryanorsinger/f77e5ec94dbe14e21771/raw/d4a1f916723ca69ac99fdcab48746c6682bf4530/profiles.json
+// then copy the JSON and assign it to a variable named profiles.
+
+// Exercise 5. Create an object named profileReport and add the following methods that use the "profiles" JSON data.
+//  getProfileCount() should return the total number of profiles
+//  getActiveCount() should return the number of active profiles
+//  getInactiveCount() should return the number of inactive profiles
+//  sumOfAllBalances() should return sum of the balance of all profiles
+//  getAverageBalance() should return the average balance per users
+//  getLowestBalance() should return the customer name with the lowest balance
+//  getHighestBalance() should return the customer name with the highest balance
+//  getMostFavoriteFruit() should return the most common fruit
+//  getLeastFavoriteFruit() should return the least favorite fruit
+//  getTotalNumberOfUnreadMessages() should return the number of unread messages for all users
+//  getAverageNumberOfUnreadMessages() should return the average number of unread mesages per user.
+//  getAverageAge() should return the average age of all users
+//  getGenderCounts() should return gender count of users as an object: example {"m": 23, "f", 32} 
+//  getAllCompanyNames() should return an array of all companies represented by the users
+//  getMostCommonEyeColor() should return the most commonly occuring eye-color.
+//  getBalancesForActiveAndNonActive() should return the balance of all non-active accounts vs. the balance of all active accounts?
+//     this last method should return an object that looks like {"active-balances": 23000, "inactive-balances": 4000} w/ different numbers.
+
+
+// Exercise 6. Practice with assignment by reference 
+// create a variable named person1 with a name property. Assign it a name property.
+// Now create a variable named person2 and assign person1 to it.
+// Then reassign the name property on person2 with person2.name = "Bob";
+// console.log the name property on person 1.
+// Why do you think changing person2 altered person1?
+// The term for this behavior is assignment by reference.
+
+
+// Exercise 7. More practice with assignment by reference
+// Add the following HTML to your index.html page in this project.
+// <input type="text" id="input" name="input">
+// now, create a variable named input and assign it document.getElementById("input").
+// now run input.value = "Hello" from the JS console. Did you see any HTML change?
+// Create a second variable named inputElement.
+// var inputElement = document.getElementById("input");
+// now, run inputElement.value = "I am altering an object by altering its properties" in your JS console.
+// See how both the "input" and "textInput" varialbes are referencing the same underlying HTML element?
+// This behavior is another example of "assignment by reference"
