c9ce64c63ccdf88aac3cd5db0a7a2a5cd791f0ff
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-10-15 15:44:23 -0500

    All bonuses except 3

--- js/dom-bonuses.js
+++ js/dom-bonuses.js
@@ -6,13 +6,31 @@ do {
     shoplist.push(groceryList);
 } while (groceryList !== null);
 
-function makeUL(array) {
-    var list = document.createElement('ul');
-    for (var i = 0; i < array.length; i++) {
-        var item = document.createElement('li');
-        item.appendChild(document.createTextNode(array[i]));
-        list.appendChild(item);
+function makeList() {
+    var listData = shoplist.slice(0,shoplist.length -1);
+    var listContainer = document.createElement('div');
+    document.getElementsByTagName('body')[0].appendChild(listContainer);
+    var listElement = document.createElement('ul');
+    listContainer.appendChild(listElement);
+    var numberOfListItems = listData.length;
+    for (var i = 0; i < numberOfListItems; ++i) {
+        var listItem = document.createElement('li');
+        listItem.innerHTML = listData[i];
+        listElement.appendChild(listItem);
     }
-    return list;
 }
-document.getElementById('myList').appendChild(makeUL(shoplist));
+
+makeList();
+
+function customStyling() {
+    var usersBackgroundColor = prompt("What color would you like this page to be?");
+    var usersFontColor = prompt("What color would you like the font on this page to be?");
+    var usersFontFamily = prompt("What font would you like this page to use?");
+    var body = document.getElementsByTagName("body");
+    body[0].style.fontFamily = usersFontFamily;
+    body[0].style.backgroundColor = usersBackgroundColor;
+    body[0].style.color = usersFontColor;
+}
+
+customStyling();
+
