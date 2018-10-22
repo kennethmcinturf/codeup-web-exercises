f02e6efbf4dbc36f0229896cb298a6cb3020b253
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-10-22 10:51:29 -0500

    finished before class example

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,6 +1,25 @@
 "use strict";
 
 $(document).ready(function() {
-    $('li').css('font-size','20px');
-    $('h1, p, li')
+    // $('li').css('font-size','20px');
+    // $('p,li,h1').css('background-color','brown');
+    // var contents = $('h1').html();
+    // alert(contents);
+
+    $('h1').click(function () {
+        $(this).css('background-color','pink')
+    });
+
+    $('p').dblclick(function (e) {
+        $(this).css('font-size','18px');
+    });
+
+    $('li').hover(
+        function () {
+            $(this).css('color','red')
+        },
+        function () {
+            $(this).css('color','black')
+        }
+    );
 });
