c820b945edc9eef4cf6cc6d90024f9bd74e32462
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-10-31 12:26:32 -0500

    After going through in class

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -38,3 +38,172 @@ const users = [
     }
 ];
 
+const userThreeLanguage = users.filter(user =>{
+   return user.languages.length >= 3
+});
+
+const emails = users.map(user => {
+    return user.email
+})
+
+const yearsOfExperience = users.reduce((accumulator,user) => {
+    return user.yearsOfExperience + accumulator;
+}, 0);
+
+const longestEmail = users.reduce((accumulator,user) =>{
+    if (accumulator.length < (user.email).length){
+        return user.email
+    }else {
+        return accumulator
+    }
+}, '');
+
+const singleName = users.reduce((accumulator,user) => {
+    let names = accumulator + user.name;
+    let namesWithSentence = `${names}, `;
+    return `${namesWithSentence}`
+}, 'Your instructors are ');
+
+console.log(`${singleName.substring(0,singleName.length-2)}.`);
+
+const uniqueLangaugaes = users.reduce((uniqueArray,user) =>{
+    for (var i = 0; i < user.languages.length; i++){
+        if (uniqueArray.indexOf(user.languages[i]) === -1){
+            uniqueArray.push(user.languages[i]);
+        }
+    }
+    return uniqueArray;
+}, []);
+
+console.log(uniqueLangaugaes);
+
+// const uniqueLangaugaes = users.reduce((accumulator,user) =>{
+//     return `${accumulator},${user.languages}`
+// }, '')
+//
+// let uniqueLangauages2 = uniqueLangaugaes.replace(',','').split(',')
+//
+//
+// console.log(uniqueLangauages2);
+//
+// const theFinalString = uniqueLangauages2.filter((language, index) =>{
+//     return uniqueLangauages2.indexOf(language) == index;
+// })
+
+// ======= BONUS 1 Problems
+const fruits = ["cantaloupe", "orange", "date", "elderberry", "ugli fruit", "pineapple"];
+const customers = [
+    {
+        name: "Fred",
+        age: 58,
+        occupation: "Police Officer",
+        noOfPurchases: 4
+    },
+    {
+        name: "Samantha",
+        age: 54,
+        occupation: "Teacher",
+        noOfPurchases: 18
+    },
+    {
+        name: "Charles",
+        age: 38,
+        occupation: "Librarian",
+        noOfPurchases: 9
+    }
+];
+// *** hints for these problems and additional bonuses are below
+// PROBLEM 1 - create an array of the first letters of each fruit
+const firstLetter = fruits.map(fruit => {
+    return fruit.charAt(0);
+});
+
+// console.log(firstLetter);
+
+// PROBLEM 2 - create array of user objects based on the customers array
+// of objects (each user object should just have name and age properties)
+const nameAndAge = customers.map(customer => {
+    return {name: customer.name, age: customer.age}
+});
+
+// console.log(nameAndAge);
+
+// PROBLEM 3 - create an array of civil servant customers (teachers and police officers)
+// containing the same properties as the objects on the customers objects
+const civilServants = customers.filter(customer => {
+    if (['Police Officer','Teacher'].includes(customer.occupation)){
+        return {name: customer.name, age: customer.age}
+    }
+})
+console.log(civilServants);
+// PROBLEM 4 - determine the average age of customers
+const totalAge = customers.reduce((accumulator,customer) =>{
+    return accumulator + customer.age
+}, 0)
+const averageAge = totalAge / customers.length;
+console.log(averageAge);
+//            PROBLEM 1 HINT - use .map()
+//            PROBLEM 2 HINT - use .map()
+//            PROBLEM 3 HINT - use .filter()
+//            PROBLEM 4 HINT - use .reduce()
+// ======= BONUS 2 Problems
+// Given the following array...
+const names = ["John", "Max", "Ronald"];
+// complete the bonuses below...
+// - Create an array where all names are given a last name of Smith.
+const lastName = names.map(name => {
+    return `${name} Smith`
+})
+// console.log(lastName);
+// - Create an array where each word is in all caps
+const caps = names.map(name => {
+    return name.toUpperCase()
+})
+
+// console.log(caps);
+// - Create an array where all names have more than 3 letters
+const threeLetter = names.filter(name => {
+    return name.length > 3
+})
+
+// console.log(threeLetter);
+// - Create an array of names with only the last two letters of each name
+const lastTwoLetter = names.map(name => {
+    return name.substring(name.length - 2, name.length)
+});
+
+console.log(lastTwoLetter);
+// - Create a total count of all letters
+
+// const countLetters = names.reduce((total,))
+// - Create a string of all letters in alphabetical order
+// - Create an array of word objects with properties of wordLength, firstLetter, lastLetter
+// - Create a string of all vowels in the entire array of names
+// - Create a single object with properties
+// ======= BONUS 3 Problems
+// Given the following array...
+const family = [
+    {
+        name: "Karen",
+        gender: "female",
+        age: 29,
+    },
+    {
+        name: "Summer",
+        gender: "female",
+        age: 10,
+    },
+    {
+        name: "Bob",
+        gender: "male",
+        age: 32,
+    },
+];
+// complete the bonuses below...
+// - Calculate the average age of family members
+// - Create an array of family objects without the age property
+// - Create an array of all minors
+// - Calculate the total age combined of family members
+// - Create an array of only female family member objects
+// - Create a single object with properties containing arrays of all names, genders, and ages
+
