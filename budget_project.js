695a527a57cab0c7e684b0033c19bf2967ec5dbd
KennethMcInturf <kennethcmcinturf@gmail.com>
2019-01-02 14:14:28 -0600

    that snake game I decided not to do

--- budget_project.js
+++ budget_project.js
@@ -1,11 +1,12 @@
 "use strict";
 
 let buildSentence = (amount,input) =>{
+    let pay = createPayValue();
     let label = $("label[for='"+input+"']");
-    let html = `<div class="row">`;
+    let html = `<div class="row justify-content-center">`;
     html += `<div class="col-8 text-center">`;
     html += `<p>`;
-    html += `The amount budgeted for ${label.html()} this will be $${amount}`;
+    html += `The amount budgeted for ${label.html()} this will be $${amount}, accounting for ${(amount/pay) * 100}% of your monthly income`;
     html += '</p>';
     html += `</div>`;
     html += `</div>`;
@@ -13,17 +14,10 @@ let buildSentence = (amount,input) =>{
 };
 
 let buildSaveSentence = (cost) => {
-    let pay = $('#pay').val();
-    let payoften = $('#pay_often').val();
-    if (payoften === "bi-weekly") {
-        pay = pay * 2;
-    }else if (payoften === "weekly"){
-        pay = pay * 4;
-    }
-    let html = `<div class="row">`;
+    let html = `<div class="row justify-content-center">`;
     html += `<div class="col-8 text-center">`;
     html += `<p>`;
-    html += `The amount you will have left over in savings is $${pay - cost}`;
+    html += `The amount you will have left over in savings is $${cost}`;
     html += '</p>';
     html += `</div>`;
     html += `</div>`;
@@ -38,6 +32,43 @@ let addBudgetItem = (value) => {
     html += `<input type="text" id="${valueWithoutWhiteSpace}" class="${value} budgetItem">`;
     html += `</div>`;
     return html;
+};
+
+let addPayItem = (value) => {
+    let valueWithoutWhiteSpace = value.replace(/\s/g, '');
+    let html = '';
+    html += `<div class="row">`;
+    html += `<div class="col-6 text-center border-right border-black">`;
+    html += `<label for="pay${valueWithoutWhiteSpace}">How often do you get paid?</label><br>`;
+    html += `<select id="pay${valueWithoutWhiteSpace}">`;
+    html += '<option value="weekly">Weekly</option>';
+    html += '<option value="bi-weekly">Bi-Weekly</option>';
+    html += '<option value="monthly">Monthly</option>';
+    html+= ` </select>`;
+    html += `</div>`;
+    html += `<div class="col-6 text-center">`;
+    html += `<label for="${valueWithoutWhiteSpace}">${value}</label><br>`;
+    html += `<input type="text" id="${valueWithoutWhiteSpace}" class="payItem">`;
+    html += `</div>`;
+    html += `</div>`;
+    return html;
+};
+
+let buildPayValue = () => {
+    let pay = [];
+    $('.payItem').each(function () {
+        let id = $(this).attr('id');
+        let val = $(this).val();
+        console.log($(`#${"pay" + id}`));
+        if ($(`#${"pay" + id}`).val() === "weekly"){
+            pay.push(parseFloat(val) * 4);
+        }else if ($(`#${"pay" + id}`).val() === "bi-weekly"){
+            pay.push(parseFloat(val) * 2);
+        }else {
+            pay.push(parseFloat(val));
+        }
+    });
+    return pay;
 };
 
 let buildBudgetArray = () => {
@@ -58,9 +89,29 @@ let createValueArray = () =>{
     return valueArray;
 };
 
-let createGraph = () => {
-    let budgetArray = buildBudgetArray();
+let createPayValue = () => {
+    let payArray = buildPayValue();
+    let pay = 0;
+    payArray.forEach(function (element) {
+        pay = pay + parseFloat(element);
+    });
+    return pay;
+};
+
+let createCostValue = () => {
+    let cost = 0;
     let valueArray = createValueArray();
+    valueArray.forEach(function (element) {
+        cost = cost + parseFloat(element);
+    });
+    return cost;
+};
+
+let createGraph = () => {
+    let valueArray = createValueArray();
+    let budgetArray = buildBudgetArray();
+    let pay = createPayValue();
+    let cost = createCostValue();
     google.charts.load('current', {'packages':['corechart']});
     google.charts.setOnLoadCallback(drawChart);
     function drawChart() {
@@ -70,6 +121,12 @@ let createGraph = () => {
         for (let i = 0; i < budgetArray.length; i++){
             data.addRows([
                 [budgetArray[i], parseFloat(valueArray[i])]
+            ])
+        }
+        pay = pay - cost;
+        if (pay > 0){
+            data.addRows([
+                ['saving', pay]
             ])
         }
         let options = {
@@ -85,17 +142,23 @@ let createGraph = () => {
 
 $('#budgetBreakdown').click(function () {
     let budgetArray = buildBudgetArray();
-    let cost = 0;
+    let cost = createCostValue();
+    let pay = createPayValue();
     budgetArray.forEach(function (element) {
         if ($(`#${element}`).val() === ""){
             return;
         }
-        cost = cost + parseFloat($(`#${element}`).val());
         $('#sentences').append(buildSentence($(`#${element}`).val(), `${element}`));
     });
-    $(`#sentences`).append(buildSaveSentence(cost));
+    $(`#sentences`).append(buildSaveSentence((pay - cost)));
     $('#budgetBreakdown').off();
     createGraph();
+});
+
+$('#payAddItem').click(function () {
+    $('#hiddenContentPay').attr('hidden', true);
+    $('#thePlaceToAddPayItems').append(addPayItem($('#payItemToAdd').val()));
+    $('#payItemToAdd').val('');
 });
 
 $('#addBudgetItem').click(function () {
@@ -106,4 +169,8 @@ $('#addItem').click(function () {
     $('#hiddenContent').attr('hidden', true);
     $('#thePlaceToPutNewBudgetItems').append(addBudgetItem($('#itemToAdd').val()));
     $('#itemToAdd').val('');
+});
+
+$('#addPayItem').click(function () {
+    $('#hiddenContentPay').removeAttr('hidden');
 });
---
+++ snake-game/snake_game.js
@@ -0,0 +1,3 @@
+let canvas = $("game_board");
+let snake = [];
+
